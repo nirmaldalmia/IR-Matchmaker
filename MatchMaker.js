0513590209295cecbcbebeb5f0b40b19674c4426
nirmaldalmia <nirmaldalmia17@gmail.com>
2018-11-26 00:02:56 +0530

    Updated database an added user registration module

--- MatchMaker.js
+++ MatchMaker.js
@@ -11,7 +11,7 @@ const personality_insights = new PersonalityInsightsV3({
 const PersonalityTextSummaries = require('personality-text-summary');
 var fs = require("fs");
 var data = require('./data.json');
-// var personality = require('./personality.json');
+var user = require('./user.json');
 const v3EnglishTextSummaries = new PersonalityTextSummaries({
   locale: 'en',
   version: 'v3'
@@ -22,15 +22,22 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-PersonalityProfiling();
+rl.question('What is your gender? Male or Female?: ', (gender) => {
+    rl.question('Enter a description of yourself: ', (about) => {
+        user[1].about = about;
+        user[1].gender = gender;
+        about = aboutExpand(user[1].about, 1);
+    })
+})
+// PersonalityProfiling();
 
 function writeToFile() {
-  fs.writeFile("./personality.json", JSON.stringify(data, null, 4), (err) => {
+  fs.writeFile("./user.json", JSON.stringify(user, null, 4), (err) => {
     if (err) {
       console.error(err);
       return;
     };
-    console.log("Personality profiling complete!");
+    console.log("Successfully created user!");
   });
 }
 
@@ -43,12 +50,12 @@ function aboutExpand(about, i) {
   return about;
 }
 
-function PersonalityProfiling() {
-  var i;
-  for (i = 1; i <= 5; i++) {
-    about = aboutExpand(data[i].about, i);
-  }
-}
+// function PersonalityProfiling() {
+//   var i;
+//   for (i = 1; i <= 5; i++) {
+//     about = aboutExpand(data[i].about, i);
+//   }
+// }
 
 function PersonalityAnalysis(about, i) {
   let params = {
@@ -65,13 +72,13 @@ function PersonalityAnalysis(about, i) {
       var personalityText = getTextSummary(response);
     console.log(personalityText);
     const personalityKeywords = sw.removeStopwords(personalityText.split(" "));
-    data[i].personality = personalityText;
-    data[i].personalityKeywords = personalityKeywords;
+    user[i].personality = personalityText;
+    user[i].personalityKeywords = personalityKeywords;
     var personalityStemmed = [];
     for (var j = 0; j < personalityKeywords.length; j++) {
       personalityStemmed.push(stemmer(personalityKeywords[j]))
     }
-    data[i].personalityStemmed = personalityStemmed;
+    user[i].personalityStemmed = personalityStemmed;
     writeToFile();
     return (personalityText);
   });
